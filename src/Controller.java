a7b22df5079121f9d88f8be64215ee0730f616bf
Valeriya Vardanyan <valeriyavardanyan@gmail.com>
2023-11-10 00:17:59 -0500

    upload button adds new selectable files

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,17 @@
 package src;
 
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Controller {
@@ -57,6 +63,9 @@ public class Controller {
     private Button uploadButton;
 
     @FXML
+    private ListView formListView;
+
+    @FXML
     private ScrollPane fileNameListScrollPane;
 
     @FXML
@@ -75,18 +84,46 @@ public class Controller {
     @FXML
     private Label rightStatusLabel;
 
+    List<String> fileNames = new ArrayList<>();
+    int i = 0;
+    List<ListItem> fileList= new ArrayList<>();
     @FXML
     private void initialize() {
-        // You can perform initialization tasks here
-        uploadButton.setOnAction(actionEvent -> System.out.println("upload"));
+        uploadButton.setOnAction(actionEvent -> {
+            fileNames.add("file" + i + ".pdf");
+            populateFileNameList();
+            i++;
+        });
         downloadAllButton.setOnAction(actionEvent -> System.out.println("download all"));
         generateButton.setOnAction(actionEvent -> System.out.println("generate"));
-
+        populateFileNameList();
     }
 
-    // Add methods or event handlers as needed
 
 
+    private void populateFileNameList() {
+
+        formListView.getItems().clear();
+        for (String fileName : fileNames) {
 
 
+            CheckBox checkBox = new CheckBox();
+            checkBox.setOnAction(actionEvent -> handleCheckBox(checkBox, fileName));
+            Label label = new Label(fileName);
+
+            HBox hbox = new HBox(checkBox, label);
+            ListItem newListItem = new ListItem(checkBox,label);
+            fileList.add(newListItem);
+            formListView.getItems().add(hbox);
+
+        }
+    }
+
+    private void handleCheckBox(CheckBox checkBox, String fileName) {
+        if (checkBox.isSelected()) {
+            System.out.println(fileName + " Selected");
+        } else {
+            System.out.println(fileName + " Un-Selected");
+        }
+    }
 }
