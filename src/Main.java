809d3a16fe7f4aca4562f90bff926c10a290a452
Tyrell Robbins <35437995+Tyrell1052@users.noreply.github.com>
2023-11-09 23:34:19 -0500

    implemented initial screen FXMLLoader

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 package src;
-
 public class Main {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(""));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("Form FLow");
+        stage.show();
+    }
     public static void main(String[] args){
-        System.out.println("HELLOW");
+        launch(args);
     }
 }
