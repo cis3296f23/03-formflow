a709121b9fca1da2061594c7f3184ca800522463
Valeriya Vardanyan <valeriyavardanyan@gmail.com>
2023-11-09 22:20:34 -0500

    FMXL Component 1

---
+++ src/FileManager.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    public static List<String> getFileNames(){
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Blue");
+        list.add("Red");
+        list.add("Background");
+        list.add("Bard");
+
+        return list;
+    }
+}
